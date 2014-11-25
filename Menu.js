14f59cb4f26046b47b32763450136f1ffbab9f92
Jaimelenn <jaimeu@uw.edu>
2014-11-25 09:43:11 -0800

    first commit

---
+++ Menu.js
@@ -0,0 +1,37 @@
+Menu
+
+Name
+Price
+
+
+
+Category  :
+	Appetizer
+	Soup
+	Congee
+	Noodle Soup
+	Chicken
+	Beef
+	Pork
+	Seafood
+	Tofu
+	Vegetarian
+	Hot Pot
+	Noodle
+	Rice
+	Drinks
+	Spicy
+
+
+var menu = [
+ 	{
+ 		"name":
+ 		"price" :
+ 		"category" :
+
+ 	}
+
+]
+
+
+
