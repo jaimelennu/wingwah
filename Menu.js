fd66892f1667d644822b51ba5a0b362d2c921e78
Jaimelenn <jaimeu@uw.edu>
2014-11-26 17:49:58 -0800

    me and rosannas menu items added

--- Menu.js
+++ Menu.js
@@ -5,33 +5,670 @@ Price
 
 
 
-Category  :
-	Appetizer
-	Soup
-	Congee
-	Noodle Soup
-	Chicken
-	Beef
-	Pork
-	Seafood
-	Tofu
-	Vegetarian
-	Hot Pot
-	Noodle
-	Rice
-	Drinks
-	Spicy
+Category:
+   Appetizer
+Soup
+Congee
+Noodle Soup
+Chicken
+Beef
+Pork
+Seafood
+Tofu
+Vegetarian
+Hot Pot
+Noodle
+Rice
+Drinks
+Spicy
 
 
-var menu = [
- 	{
- 		"name":
- 		"price" :
- 		"category" :
+var menu = [{
+      "name": "Szechuan Pickled Cucumber",
+      "price": "$2.95",
+      "category": [
+         "appetizer", "spicy"
+      ]
 
- 	}
+   }, {
+      "name": "Simmered Bean Curd",
+      "price": "$5.25",
+      "category": [
+         "appetizer"
+      ]
+   }, {
+      "name": "Simmered Seaweed",
+      "price": "$2.95",
+      "category": [
+         "appetizer"
+      ]
+   }, {
+      "name": "Fried Spring Roll (2)",
+      "price": "$2.75",
+      "category": [
+         "appetizer"
+      ]
+   }, {
+      "name": "Free Range Chicken",
+      "price": "$5.25",
+      "category": [
+         "appetizer", "chicken"
+      ]
+   }, {
+      "name": "Deep Fried Tofu",
+      "price": "$5.50",
+      "category": [
+         "appetizer", "tofu"
+      ]
+   }, {
+      "name": "Steamed of Fried Pot Stickers",
+      "price": "$5.25",
+      "category": [
+         "appetizer"
+      ]
+   }, {
+      "name": "Deep Fried Fish Ball with Homemade Sauce",
+      "price": "$3.95",
+      "category": [
+         "appetizer", "spicy"
+      ]
+   }, {
+      "name": "Popcorn Chicken",
+      "price": "$5.25",
+      "category": [
+         "appetizer", "chicken", "spicy"
+      ]
+   }, {
+      "name": "Fried Wonton",
+      "price": "$4.25",
+      "category": [
+         "appetizer"
+      ]
+   }, {
+      "name": "Wonton with Chili Sauce",
+      "price": "$4.95",
+      "category": [
+         "appetizer", "spicy"
+      ]
+   }, {
+      "name": "Steamed or Deep Fried Bun",
+      "price": "$3.95",
+      "category": [
+         "appetizer"
+      ]
+   }, {
+      "name": "Deep Fried Taiwanese Pork Chop (1 Piece)",
+      "price": "$2.95",
+      "category": [
+         "appetizer", "pork"
+      ]
+   }, {
+      "name": "Fried Chicken Wing",
+      "price": "$5.95",
+      "category": [
+         "appetizer", "chicken"
+      ]
+   }, {
+      "name": "BBQ Pork",
+      "price": "$5.95",
+      "category": [
+         "appetizer", "pork"
+      ]
+   }, {
+      "name": "Deep Fried Squid with Salt & Pepper",
+      "price": "$8.95",
+      "category": [
+         "appetizer", "spicy", "seafood"
+      ]
+
+      {
+         "name": "Five Spicy Flavored Hard",
+         "price": "$6.95",
+         "category": [
+            "appetizer", "spicy", "tofu", "pork"
+         ]
+      },
+      {
+         "name": "Egg Corn Soup",
+         "price": "$5.25",
+         "category": [
+            "soup"
+         ]
+      },
+      {
+         "name": "Hot and Sour Soup",
+         "price": "$5.25",
+         "category": [
+            "soup", "spicy"
+         ]
+      },
+      {
+         "name": "Wonton Soup",
+         "price": "$5.95",
+         "category": [
+            "soup"
+         ]
+      },
+      {
+         "name": "Fish Ball Soup",
+         "price": "$5.95",
+         "category": [
+            "soup", "seafood"
+         ]
+      },
+      {
+         "name": "Beef Ball Soup",
+         "price": "$5.95",
+         "category": [
+            "soup", "beef"
+         ]
+      },
+      {
+         "name": "Minced Beef with Egg White Soup",
+         "price": "$6.95",
+         "category": [
+            "soup", "beef"
+         ]
+      },
+      {
+         "name": "Chicken Cream Corn Soup",
+         "price": "$5.95",
+         "category": [
+            "soup", "chicken"
+         ]
+      },
+      {
+         "name": "Salted Egg, Pork and Green Mustard Soup",
+         "price": "$6.95",
+         "category": [
+            "soup", "pork"
+         ]
+      },
+      {
+         "name": "Seafood Hot & Sour Soup",
+         "price": "$6.95",
+         "category": [
+            "soup", "seafood", "spicy"
+         ]
+      },
+      {
+         "name": "House Special Wonton Soup",
+         "price": "$6.95",
+         "category": [
+            "soup"
+         ]
+      },
+      {
+         "name": "Seafood Tofu Soup",
+         "price": "$7.95",
+         "category": [
+            "soup", "seafood", "tofu"
+         ]
+      },
+      {
+         "name": "Chicken and Fish Maw Soup",
+         "price": "$8.95",
+         "category": [
+            "soup", "chicken", "seafood"
+         ]
+      },
+      {
+         "name": "Corn and Fish Maw Soup",
+         "price": "$8.95",
+         "category": [
+            "soup", "seafood"
+         ]
+      },
+      {
+         "name": "Preserved Egg and Pork Congee",
+         "price": "$4.25",
+         "category": [
+            "congee", "pork"
+         ]
+      },
+      {
+         "name": "Beef Congee",
+         "price": "$4.25",
+         "category": [
+            "congee", "beef"
+         ]
+      },
+      {
+         "name": "Chicken Black Mushroom Congee",
+         "price": "$4.25",
+         "category": [
+            "congee", "chicken"
+         ]
+      },
+      {
+         "name": "Fish Fillet Congee",
+         "price": "$4.75",
+         "category": [
+            "congee", "seafood"
+         ]
+      },
+      {
+         "name": "Seafood Congee",
+         "price": "$5.25",
+         "category": [
+            "congee", "seafood"
+         ]
+      },
+      "name": "Hot Vietnamese Coffee",
+      "price": "$2.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Vietnamese Coffee",
+      "price": "$2.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Hong Kong Style Milk Tea",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Hong Kong Style Milk Tea",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Hong Kong Style Coffee",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Hong Kong Style Coffee",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Coffee and Tea with Milk",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Hong Kong Style Coffee",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Fresh Ginger with Fresh Milk",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Fresh Ginger with Fresh Milk",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Fresh Ginger with Fresh Tea",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Fresh Ginger with Milk Tea",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Chocolate Milk",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Chocolate Milk",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Ovaltine",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Ovaltine",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Horlicks",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Horlicks",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Almond Drink",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Almond Drink",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Lemon with Honey Drink",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Lemon with Honey Drink",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Lemon Tea",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Lemon Tea",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Cold Lemon Coke",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Hot Lemon Coke",
+      "price": "$1.75",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Lemonade",
+      "price": "$2.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Orange Juice",
+      "price": "$2.50",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Soft Drink",
+      "price": "$1.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Soy milk",
+      "price": "$1.25",
+      "category": ["Drinks"]
+   },
+
+   {
+      "name": "Broccoli Beef",
+      "price": "$8.95",
+      "category": ["Beef"]
+   },
+
+   {
+      "name": "Broccoli Chicken",
+      "price": "$8.95",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Broccoli Pork",
+      "price": "$8.95",
+      "category": ["Pork"]
+   },
+
+   {
+      "name": "Mongolian Beef",
+      "price": "$8.95",
+      "category": ["Beef", "Spicy"]
+   },
+
+   {
+      "name": "Mongolian Chicken",
+      "price": "$8.95",
+      "category": ["Chicken", "Spicy"]
+   },
+
+   {
+      "name": "Szechuan Chicken",
+      "price": "$8.95",
+      "category": ["Chicken", "Spicy"]
+   },
+
+   {
+      "name": "Szechuan Beef",
+      "price": "$8.95",
+      "category": ["Beef", "Spicy"]
+   },
+
+   {
+      "name": "Szechuan Pork",
+      "price": "$8.95",
+      "category": ["Pork", "Spicy"]
+   },
+
+   {
+      "name": "Kung Pao Chicken",
+      "price": "$8.95",
+      "category": ["Spicy", "Chicken"]
+   },
+
+   {
+      "name": "Kung Pao Beef",
+      "price": "$8.95",
+      "category": ["Beef", "Spicy"]
+   },
+
+   {
+      "name": "Vegatable with Beef",
+      "price": "$8.95",
+      "category": ["Beef"]
+   },
+
+   {
+      "name": "Vegatable with Chicken",
+      "price": "$8.95",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Vegatable with Pork",
+      "price": "$8.95",
+      "category": ["Pork"]
+   },
+
+   {
+      "name": "Orange Beef",
+      "price": "$9.50",
+      "category": ["Beef", "Spicy"]
+   },
+
+   {
+      "name": "Sesame Beef",
+      "price": "$9.50",
+      "category": ["Beef"]
+   },
+
+   {
+      "name": "Cashew Nut Chicken",
+      "price": "$8.95",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Sweet and Sour Chicken",
+      "price": "$8.95",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Chicken with Lemon Sauce Glaze",
+      "price": "$8.95",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Curry Chicken",
+      "price": "$8.95",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Curry Beef",
+      "price": "$8.95",
+      "category": ["Beef", "Spicy"]
+   },
+
+   {
+      "name": "Thai Style Basil Beef",
+      "price": "$8.95",
+      "category": ["Beef", "Spicy"]
+   },
+
+   {
+      "name": "Thai Style Basil Chicken",
+      "price": "$8.95",
+      "category": ["Chicken", "Spicy"]
+   },
+
+   {
+      "name": "Thai Style Basil Pork",
+      "price": "$8.95",
+      "category": ["Pork", "Spicy"]
+   },
+
+   {
+      "name": "String Bean with Beef",
+      "price": "$8.95",
+      "category": ["Beef"]
+   },
+
+   {
+      "name": "String Bean with Pork",
+      "price": "$8.95",
+      "category": ["Pork"]
+   },
+
+   {
+      "name": "String Bean with Chicken",
+      "price": "$8.95",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "General Tso Chicken",
+      "price": "$9.50",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Orange Chicken",
+      "price": "$9.50",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Sesame Chicken",
+      "price": "$9.50",
+      "category": ["Chicken"]
+   },
+
+   {
+      "name": "Simmered Bean Curd with Pork",
+      "price": "$9.50",
+      "category": ["Pork"]
+   },
+
+   {
+      "name": "Beef with Oyster Sauce",
+      "price": "$8.95",
+      "category": ["Beef"]
+   },
+
+   {
+      "name": "Salt and Pepper Pork Chop",
+      "price": "$8.95",
+      "category": ["Pork", "Spicy"]
+   },
+
+   {
+      "name": "Peking Pork Chop",
+      "price": "$8.95",
+      "category": ["Pork"]
+   },
+
+   {
+      "name": "Pork Chop with Black Bean Sauce",
+      "price": "$8.95",
+      "category": ["Pork"]
+   },
+
+   {
+      "name": "Honey Pork Chop with Garlic",
+      "price": "$8.95",
+      "category": ["Pork"]
+   },
+
+   {
+      "name": "Black Peppered Pork Chop",
+      "price": "$8.95",
+      "category": ["Pork"]
+   },
+
+   {
+      "name": "XO Sauce Beef",
+      "price": "$9.95",
+      "category": ["Beef"]
+   },
+
+   {
+      "name": "Three Kinds of Meat Sautéed with House Sauce",
+      "price": "$9.50",
+      "category": [
+         "chicken", "pork", "beef"
+      ]
+   },
+
+   {
+      "name": "Szechuan Shredded Pork",
+      "price": "$8.95",
+      "category": ["Pork"]
+   },
+   //add more here
 
 ]
-
-
 
