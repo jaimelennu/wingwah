207176662a5b3e76e82a4be6c1f246330bd32b5d
rtduong <rtduong@uw.edu>
2014-11-30 10:16:48 -0800

    Added Raymond's Sections
    House Special Noodle, Hot Pot, Tofu and Vegetable

--- Menu.js
+++ Menu.js
@@ -14,11 +14,9 @@ Chicken
 Beef
 Pork
 Seafood
-Tofu
-Vegetarian
+Tofu and Vegetable
 Hot Pot
-Noodle
-Rice
+House Special Noodle/Rice
 Drinks
 Spicy
 
@@ -658,9 +656,7 @@ var menu = [{
    {
       "name": "Three Kinds of Meat Sautéed with House Sauce",
       "price": "$9.50",
-      "category": [
-         "chicken", "pork", "beef"
-      ]
+      "category": ["Chicken", "Pork", "Beef"]
    },
 
    {
@@ -668,6 +664,433 @@ var menu = [{
       "price": "$8.95",
       "category": ["Pork"]
    },
+   
+   {
+      "name": "Free Range Chicken over Rice",
+      "price": "$6.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "B.B.Q Pork & Tofu over Rice",
+      "price": "$5.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Five Spicy Flavored Hard Boiled Pork Belly over Rice",
+      "price": "$6.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Three Kind Hard Boiled over Rice",
+      "price": "$7.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Taiwanese Pork Chop over Rice",
+      "price": "$6.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Teriyaki Chicken over Rice",
+      "price": "$6.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Lemon Grass Pork Chop over Rice",
+      "price": "$6.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Lemon Grass Chicken over Rice",
+      "price": "$6.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Beef, Chicken, Shrimp with Egg over Rice",
+      "price": "$6.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Curry Chicken or Beef over Rice",
+      "price": "$6.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Curry Fish or Squid over Rice",
+      "price": "$6.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Chicken or Fish Cream Corn over Rice",
+      "price": "$6.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Seafood over Rice",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Popcorn Chicken over Rice",
+      "price": "$6.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Brown Beef and Tendon over Rice",
+      "price": "$6.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Curry Tofu over Rice",
+      "price": "$5.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Grilled Beef Short Rib over Rice",
+      "price": "$7.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Grilled Beef Short Rib and Chicken over Rice",
+      "price": "$7.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Fried Rice Choice: Chicken, Pork, Beef, BBQ Pork or Vegetable",
+      "price": "$6.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Singapore Style Fried Rice Choice: Chicken, Pork, Beef, BBQ Pork or Vegetable",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Shrimp Fried Rice",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Young Chow Friend Rice",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "House Special Friend Rice",
+      "price": "$7.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Preserved Meat Fried Rice",
+      "price": "$7.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Minced Beef or Chicken Fried Rice",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Salt Fish and Diced Chicken or Pork Fried Rice",
+      "price": "$8.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Pineapple and Diced Chicken Fried Rice",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Seafood Fried Rice",
+      "price": "$8.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Preserved Meat Sticky Rice",
+      "price": "$8.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Pork Chow Fried Rice",
+      "price": "$10.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Dried Scallop Egg White Fried Rice",
+      "price": "$10.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Lover Bird's Fried Rice",
+      "price": "$10.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Cashew Nut and Honey Walnut Fried Rice",
+      "price": "$7.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Singapore Style Vermicelli",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Beef or Chicken Chow Fun",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Bean Sprouts and Soy Sauce Chow Mein",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Tofu and Vegetable Pan Fried Noodle",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Beef Chow Fun with Gravy",
+      "price": "$7.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Seafood Pan Fried Noodles",
+      "price": "$7.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "House Special Chow Fun",
+      "price": "$7.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Malaysian Style Chow Fun",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Beef or Chicken with Egg Chow Fun",
+      "price": "$7.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Pork Chow Style Vermicelli",
+      "price": "$6.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Shanghai Style Chow mein",
+      "price": "$7.25",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Pickled Cabbage and Pork with Rice Cake",
+      "price": "$7.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Shanghai Style Rice Cake",
+      "price": "$7.95",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Pickled Cabbage and Pork with Vermicelli",
+      "price": "$7.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Black Mushroom in Braised E Fu Noodles",
+      "price": "$8.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Seafood Braised E Fu Noodles",
+      "price": "$9.50",
+      "category": ["House Special Noodle/Rice"]
+   },
+   
+   {
+      "name": "Combination Tofu",
+      "price": "$8.95",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Seafood Tofu",
+      "price": "$9.50",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Beef Tendon with Vegetable or Curry Sauce",
+      "price": "$8.95",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Black Mushroom and Chicken",
+      "price": "$8.95",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Diced Chicken and Salted Fish Tofu",
+      "price": "$8.95",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Satay Beef with Vermicelli",
+      "price": "$8.95",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Five Spicy Flavored Hard Boiled Pork Belly and Tofu",
+      "price": "$8.95",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Curry Fish Ball and Vegetable",
+      "price": "$8.95",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Fish Fillet with Vermicelli",
+      "price": "$8.95",
+      "category": ["Hot Pot"]
+   },
+   
+   {
+      "name": "Deep Fried Tofu Choice: Mongolian Sauce, Kung Pao, Curry, Salt and Pepper",
+      "price": "$7.25",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Mixed Vegetable",
+      "price": "$7.25",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Ma Po Tofu",
+      "price": "$7.25",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Braised Tofu and Vegetable",
+      "price": "$7.25",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Family Tofu",
+      "price": "$7.25",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "XO Sauce Tofu",
+      "price": "$7.25",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Lettuce with Chili Bean Curd Sauce",
+      "price": "$6.95",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Egg Plant Szechuan Garlic or Thai Basil Style",
+      "price": "$7.25",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "String Bean Choice: XO Sauce, Szechuan Garlic or Black Bean Sauce",
+      "price": "$7.95",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Black Mushroom with Vegetable",
+      "price": "$8.50",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Seasonal Vegetable",
+      "price": "$7.95",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Vegetable with Oyster Sauce",
+      "price": "$4.95",
+      "category": ["Tofu and Vegetable"]
+   },
+   
+   {
+      "name": "Sugar Pea Vine",
+      "price": "$9.95",
+      "category": ["Tofu and Vegetable"]
+   },
+   
    //add more here
 
 ]
